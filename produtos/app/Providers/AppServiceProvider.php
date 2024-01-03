304db43741d672e51e93fd9d16e07d102776c28a
william rodrigues <williamr723@gmail.com>
2024-01-03 16:45:27 -0300

    deploy

--- produtos/app/Providers/AppServiceProvider.php
+++ produtos/app/Providers/AppServiceProvider.php
@@ -2,27 +2,18 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+    // ...
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
+
